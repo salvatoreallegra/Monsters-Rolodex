e0108b619fafaf11bea5521521ad0e22baef514e
Salvatore Allegra <salallegra@gmail.com>
2020-04-14 21:11:40 -0700

    Updates

---
+++ precache-manifest.8b8f048fadc459e84f2d58582381ecc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bf4a18595f0428a0d81d97f56ceeffa",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "50c62b053a388720e0df",
+    "url": "/Monsters-Rolodex/static/css/main.28ca97a1.chunk.css"
+  },
+  {
+    "revision": "05f7ab08950fb12f8f75",
+    "url": "/Monsters-Rolodex/static/js/2.1363812b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.1363812b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50c62b053a388720e0df",
+    "url": "/Monsters-Rolodex/static/js/main.6101eb09.chunk.js"
+  },
+  {
+    "revision": "d92c59ecab1be31e7a67",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.a47f78f7.js"
+  }
+]);
